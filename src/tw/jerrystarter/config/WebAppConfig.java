ce3b04fb6a59f7f47fc48add31ed212821633234
jerry <jerryoop66@gmail.com>
2021-05-21 15:56:05 +0800

    initail commit

---
+++ src/tw/jerrystarter/config/WebAppConfig.java
@@ -0,0 +1,51 @@
+package tw.jerrystarter.config;
+
+import java.util.ArrayList;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.accept.ContentNegotiationManager;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("tw.jerrystarter")
+public class WebAppConfig extends WebMvcConfigurationSupport {
+	@Bean
+	public  ViewResolver  internalResourceViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setViewClass(JstlView.class);
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+	
+	
+	@Override 
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	     registry.addResourceHandler("/css/**")
+	             .addResourceLocations("/WEB-INF/views/css/");
+	     registry.addResourceHandler("/image/**")
+	             .addResourceLocations("/WEB-INF/views/images/");
+	}
+	
+	@Bean
+	public CommonsMultipartResolver multipartResolver() {
+		CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+		resolver.setDefaultEncoding("UTF-8");
+		resolver.setMaxUploadSize(81920000);
+		return resolver;
+	}
+	
+}
---
+++ src/tw/jerrystarter/controller/IndexController.java
@@ -0,0 +1,21 @@
+package tw.jerrystarter.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class IndexController {
+	
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping
+	public String toLoginPage() {
+		return "login";
+	}
+	
+	
+}
